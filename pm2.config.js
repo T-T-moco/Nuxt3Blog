69ccd4a431960bfd63f3c5f120943ea3427b242f
T-moco <now.or.never.1229@gmail.com>
2021-12-31 13:36:05 +0900

    [update] スペースを削除

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
        name: `Nuxt3Blog`,
-       script: '.output/server/index.mjs ',
+       script: '.output/server/index.mjs',
        cwd: `${process.env.HOME}/Nuxt3Blog`,
        env: {
          PORT: 3000
